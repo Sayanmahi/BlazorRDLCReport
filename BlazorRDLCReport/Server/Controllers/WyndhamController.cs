8295dee3b37db6583dcd9e34261f25d01c2a9a06
Sayan Mukherjee <sayanmukherjeedec812@gmail.com>
2024-06-17 21:55:37 +0530

    latest modified

--- BlazorRDLCReport/Server/Controllers/WyndhamController.cs
+++ BlazorRDLCReport/Server/Controllers/WyndhamController.cs
@@ -30,8 +30,25 @@ namespace BlazorRDLCReport.Server.Controllers
             };
             report.ReportPath = $"{this._webHostEnvironment.WebRootPath}\\Reports\\ReportMain.rdlc";
             report.SetParameters(parameter);
+            
+            var dt = new DataTable();
+            dt.Columns.Add(columnName: "Transcode");
+            dt.Columns.Add(columnName: "SumTotal");
+            string[] c = { "1-Accessories", "1860-State Sales Tax", "Package Amount" };
+            string[] subTotal = { "$344.00", "$26.66", "$504.00" };
+            int p = 0;
+            foreach(var i in c)
+            {
+                DataRow dr = dt.NewRow();
+                dr["Transcode"] = i;
+                dr["SumTotal"] = subTotal[p];
+                dt.Rows.Add(dr);
+                p = p + 1;
+            }
+            report.DataSources.Add(new ReportDataSource("dbAnother", dt));
             report.SubreportProcessing += new SubreportProcessingEventHandler(SubReportHeader);
             report.SubreportProcessing += new SubreportProcessingEventHandler(SubReportProcessing);
+
             var pdf = report.Render("PDF");
             return File(pdf, "application/pdf", "report." + "pdf");
         }
@@ -96,34 +113,38 @@ namespace BlazorRDLCReport.Server.Controllers
         }
         void SubReportProcessing(object sender,SubreportProcessingEventArgs args)
         {
-            //string code= args.Parameters["Code"].Values[0].ToString();
-            string[] c = { "1-Accessories", "1860-State Sales Tax", "Package Amount" };
-            var detailsData = new DataTable();
-            detailsData = _wyndhamService.Charges();
-            DataTable dt = new DataTable();
-            dt.Columns.Add(columnName: "Code");
-            dt.Columns.Add(columnName: "Date");
-            dt.Columns.Add(columnName: "Time");
-            dt.Columns.Add(columnName: "TransactionType");
-            dt.Columns.Add(columnName: "RoomId");
-            dt.Columns.Add(columnName: "ReservationId");
-            dt.Columns.Add(columnName: "GuestName");
-            dt.Columns.Add(columnName: "Name");
-            dt.Columns.Add(columnName: "Folio");
-            dt.Columns.Add(columnName: "Amount");
-            foreach (var i in c)
+            try
             {
-                var newdetails1 = detailsData.Select($"Code = '{i}'").CopyToDataTable();
+                string code = args.Parameters["Code"].Values[0].ToString();
+                string[] c = { "1-Accessories", "1860-State Sales Tax", "Package Amount" };
+                var detailsData = new DataTable();
+                detailsData = _wyndhamService.Charges();
+                DataTable dt = new DataTable();
+                dt.Columns.Add(columnName: "Code");
+                dt.Columns.Add(columnName: "Date");
+                dt.Columns.Add(columnName: "Time");
+                dt.Columns.Add(columnName: "TransactionType");
+                dt.Columns.Add(columnName: "RoomId");
+                dt.Columns.Add(columnName: "ReservationId");
+                dt.Columns.Add(columnName: "GuestName");
+                dt.Columns.Add(columnName: "Name");
+                dt.Columns.Add(columnName: "Folio");
+                dt.Columns.Add(columnName: "Amount");
+                var newdetails1 = detailsData.Select($"Code = '{code}'").CopyToDataTable();
                 foreach (DataRow row in newdetails1.Rows)
                 {
                     DataRow newRow = dt.NewRow();
                     newRow.ItemArray = row.ItemArray;
                     dt.Rows.Add(newRow);
                 }
+                //var newdetails = detailsData.Select($"Code = '{code}'").CopyToDataTable();
+                ReportDataSource ds = new ReportDataSource("dbMain", dt);
+                args.DataSources.Add(ds);
+            }catch(Exception e)
+            {
+
             }
-            //var newdetails = detailsData.Select($"Code = '{code}'").CopyToDataTable();
-            ReportDataSource ds = new ReportDataSource("dbMain", dt);
-            args.DataSources.Add(ds);
+
         }
     }
 }
